6556df106c0368d33467113f94b86f9f3b8c402c
ddmkchan@163.com <ddmkchan@163.com>
2015-06-26 10:25:27 +0800

    fix

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#encoding : utf-8
+#encoding=utf-8
 
 class ListNode:
     def __init__(self, x):
--- gensim_demo.py
+++ gensim_demo.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#encoding : utf-8
+# -*- coding: utf-8 -*-
 from numpy import zeros
 from scipy.linalg import svd
 #following needed for TFIDF
